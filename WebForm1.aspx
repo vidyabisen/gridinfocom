aebcfed70e49069dacc87780248744af412a2fbd
vidyabisen <vidya.bisen@gridinfocom.com>
2019-03-26 15:24:02 +0530

    hhhh

---
+++ WebForm1.aspx
@@ -0,0 +1,58 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication2AddressBook.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+       
+
+        <br />
+        Address ID&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        First Name &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        Last Name&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        Email &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        Phone Number &nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox5" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        Country&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox6" runat="server"></asp:TextBox>
+       
+
+    &nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        <br />
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Insert Address" />
+&nbsp;&nbsp;
+        <asp:Button ID="Button2" runat="server" OnClick="SerchAdd_btn" Text="Search Address" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="Button3" runat="server" Text="Update" OnClick="Button3_Click" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="Button4" runat="server" Text="Delete" OnClick="Button4_Click" />
+        <asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
+        <br />
+        <br />
+       
+
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+namespace WebApplication2AddressBook
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        SqlConnection con;
+        
+
+        public SqlConnection GetConnection()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["con"].ToString();
+
+            con = new SqlConnection(connectionString);
+            return con;
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            using (con = GetConnection())
+            {
+                using (SqlCommand com = new SqlCommand("InsertAddress",con))
+                {
+                    com.CommandType = System.Data.CommandType.StoredProcedure; 
+                   // com.Parameters.AddWithValue("@id", Convert.ToInt32(TextBox1.Text));
+                    com.Parameters.AddWithValue("@FName", TextBox2.Text);
+                    com.Parameters.AddWithValue("@LName", TextBox3.Text);
+                    com.Parameters.AddWithValue("@email", TextBox4.Text);
+                    com.Parameters.AddWithValue("@PhoneNo", TextBox5.Text);
+                    com.Parameters.AddWithValue("@Country", TextBox6.Text);
+                    con.Open();
+                    com.ExecuteNonQuery();
+                    con.Close();
+                    Response.Write("Record inserted");
+                    // Label1.Text = "Record inserted";
+                    //@id,@FName,@LName,@email,@PhoneNo,@Country
+                }
+            }
+        }
+
+        protected void SerchAdd_btn(object sender, EventArgs e)
+        {
+            using (con = GetConnection())
+            {
+                using (SqlCommand com = new SqlCommand())
+                {
+                    com.CommandText = "select * from AddressBook where Address_ID=@id";
+                    com.Connection = con;
+                    com.Parameters.AddWithValue("@id", Convert.ToInt32(TextBox1.Text));
+                    com.Connection = con;
+                    con.Open();
+                    GridView1.DataSource = com.ExecuteReader();
+                    GridView1.DataBind();
+                    con.Close();
+                }
+            }
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            using (con = GetConnection())
+            {
+                using (SqlCommand com = new SqlCommand())
+                {
+                    com.CommandText = "delete from AddressBook where Address_ID=@id";
+                    com.Connection = con;
+                    com.Parameters.AddWithValue("@id",Convert.ToInt32(TextBox1.Text));
+                    com.Connection = con;
+                    con.Open();
+                    com.ExecuteNonQuery();
+                    con.Close();
+                    Response.Write("Record deleted");
+                }
+            }
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            using (con = GetConnection())
+            {
+                using (SqlCommand com = new SqlCommand())
+                {
+                    com.CommandText = "update AddressBook set LastName=@LName where Address_ID=@id";
+                    com.Connection = con;
+                    com.Parameters.AddWithValue("@id",Convert.ToInt32(TextBox1.Text));
+                    com.Parameters.AddWithValue("@LName", TextBox3.Text);
+                    com.Connection = con;
+                    con.Open();
+                    com.ExecuteNonQuery();
+                    con.Close();
+                    Response.Write("Record Updated");
+                }
+            }
+        }
+    }
+}
